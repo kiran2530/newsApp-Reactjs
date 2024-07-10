3cb910714ea9060f0fa09e6b7840172beb1d44ab
kirannikam <nikamsarkar464@gmail.com>
2024-07-10 21:31:56 +0530

    new app structure created.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c = 'component';
+
+  render() {
+    return (
+      <div>
+        <h1>hi this is class base {this.c} </h1>
+      </div>
+    )
+  }
 }
 
-export default App;
