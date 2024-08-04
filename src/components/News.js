08696c95d1d35d8f03b8bc7cea1d7721f2b5eaa7
kirannikam <nikamsarkar464@gmail.com>
2024-08-04 23:31:58 +0530

    format document

--- src/components/News.js
+++ src/components/News.js
@@ -1,124 +1,133 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
-import Spinner from './Spinner';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
+  capatilize(title) {
+    return title.charAt(0).toUpperCase() + title.slice(1);
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: true,
+      page: 1,
+      totalResults: 0,
+    };
 
-    capatilize(title) {
-        return title.charAt(0).toUpperCase() + title.slice(1);
+    document.title = `${this.capatilize(this.props.category)} - news`;
+  }
+
+  async updateNews() {
+    this.props.setProgress(1);
+    try {
+      const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=cfabeabea85c4c488e0e92fac54ef503&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+
+      this.setState({
+        loading: true,
+      });
+
+      this.props.setProgress(10);
+
+      let data = await fetch(url);
+      this.props.setProgress(30);
+      let parseData = await data.json();
+      this.props.setProgress(70);
+      console.log(parseData);
+
+      this.setState({
+        articles: parseData.articles,
+        totalResults: parseData.totalResults,
+        loading: false,
+      });
+      this.props.setProgress(100);
+    } catch (err) {
+      console.log(err);
     }
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles : [],
-            loading : true,
-            page : 1,
-            totalResults : 0
-        }
+  }
 
-        document.title = `${this.capatilize(this.props.category)} - news`;
-    }
+  fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${
+      this.props.category
+    }&apiKey=cfabeabea85c4c488e0e92fac54ef503&page=${
+      this.state.page + 1
+    }&pageSize=${this.props.pageSize}`;
 
-    async updateNews() {
-        this.props.setProgress(1);
-        try{
-            const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=cfabeabea85c4c488e0e92fac54ef503&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ page: this.state.page + 1 });
 
-            this.setState({
-                loading : true
-            })
+    this.setState({
+      loading: true,
+    });
 
-            this.props.setProgress(10);
+    this.props.setProgress(10);
 
-            let data = await fetch(url);
-            this.props.setProgress(30);
-            let parseData = await data.json();
-            this.props.setProgress(70);
-            console.log(parseData);
+    let data = await fetch(url);
+    this.props.setProgress(30);
+    let parseData = await data.json();
+    this.props.setProgress(70);
+    console.log(parseData);
 
-            this.setState({
-                articles : parseData.articles,
-                totalResults : parseData.totalResults,
-                loading : false
-            })
-            this.props.setProgress(100);
-        }
-        catch(err) {
-            console.log(err);
-        }
-        
-    }
+    this.setState({
+      articles: this.state.articles.concat(parseData.articles),
+      totalResults: parseData.totalResults,
+      loading: false,
+    });
+    this.props.setProgress(100);
+  };
 
-    fetchMoreData = async ()=> {
-        const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=cfabeabea85c4c488e0e92fac54ef503&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+  async componentDidMount() {
+    this.updateNews();
+  }
 
-        this.setState({page : this.state.page + 1})
+  handlePreviousClick = async () => {
+    console.log("previous click");
 
-            this.setState({
-                loading : true
-            })
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
+  };
 
-            this.props.setProgress(10);
+  handleNextClick = async () => {
+    console.log("next click");
 
-            let data = await fetch(url);
-            this.props.setProgress(30);
-            let parseData = await data.json();
-            this.props.setProgress(70);
-            console.log(parseData);
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
+  };
 
-            this.setState({
-                articles : this.state.articles.concat(parseData.articles),
-                totalResults : parseData.totalResults,
-                loading : false
-            })
-            this.props.setProgress(100);
-    }
-    
-    async componentDidMount() {
-        this.updateNews();
-    }
+  render() {
+    return (
+      <div className="container my-3">
+        <h2 className="text-center " style={{ marginTop: "70px" }}>
+          {this.props.title}
+        </h2>
 
-    handlePreviousClick = async()=> {
-        console.log("previous click");
-        
-        this.setState({page : this.state.page - 1})
-        this.updateNews()
-    }
-
-    handleNextClick = async()=> {
-        console.log("next click");
-        
-        this.setState({page : this.state.page + 1})
-        this.updateNews()
-    }
-
-    render() {
-        return (
-        <div className='container my-3'>
-            <h2 className='text-center ' style={{marginTop: '70px'}}>{this.props.title}</h2>
-
-            {/* spinner component  */}
-            {this.state.loading && <Spinner/>}
-            <InfiniteScroll
-                dataLength={this.state.articles.length}
-                next={this.fetchMoreData}
-                hasMore={this.state.articles.length !== this.state.totalResults}
-                loader={<Spinner/>}
-            >
-            
-            <div className="container">
-                <div className="row">
-                    {this.state.articles.map((element)=> {
-                        return (<div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} readMoreUrl={element.url} date={element.publishedAt} />
-                        </div>)
-                    })}
-                </div>
+        {/* spinner component  */}
+        {this.state.loading && <Spinner />}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title}
+                      description={element.description}
+                      imageUrl={element.urlToImage}
+                      readMoreUrl={element.url}
+                      date={element.publishedAt}
+                    />
+                  </div>
+                );
+              })}
             </div>
-                
-            </InfiniteScroll>
-        </div>
-        )
-    }
+          </div>
+        </InfiniteScroll>
+      </div>
+    );
+  }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-    render() {
-        let {title, description, imageUrl, readMoreUrl, date} = this.props;
-        return (
-        <div className='my-3'>
-            <div className="card">
-            <img src={imageUrl ? imageUrl : "https://www.livemint.com/lm-img/img/2024/07/10/1600x900/Samsung_1720623289022_1720623294099.png"} className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <p className="card-text"><small className="text-muted">{new Date(date).toGMTString()}</small></p>
-                <a href={readMoreUrl} className="btn btn-primary">Read More</a>
-            </div>
-            </div>
+  render() {
+    let { title, description, imageUrl, readMoreUrl, date } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://www.livemint.com/lm-img/img/2024/07/10/1600x900/Samsung_1720623289022_1720623294099.png"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a href={readMoreUrl} className="btn btn-primary">
+              Read More
+            </a>
+          </div>
         </div>
-        )
+      </div>
+    );
   }
 }
